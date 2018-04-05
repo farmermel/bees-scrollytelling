32be76457bc5f39e806cfdce14167735d5e8daf6
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-04 18:26:25 -0600

    Import Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Bees and Agriculture!!!</h1>
-        </header>
+        <Header />
       </div>
     );
   }
