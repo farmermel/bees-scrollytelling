8ac8c74e32691d68f1e653b150470af3ae3ef376
Casey Dallavalle <cbdallavalle@gmail.com>
2018-04-04 20:06:39 -0600

    Add story to Impact and load into App

--- src/components/Impact/Impact.js
+++ src/components/Impact/Impact.js
@@ -1,1 +1,49 @@
-//here
+import React from 'react';
+
+const dietQuestion = () => {
+  return (
+    <form>
+      <label htmlFor="diet">What percentage of your diet do you think is provided by bees?</label>
+      <input type="text" id="died" placeholder="Enter a number between 1 and 100"/>
+      <button type="submit">Guess</button>
+    </form>
+  )
+}
+
+const dietArticle = () => {
+  return (
+    <article>
+      <h1>60% diversity of food you eat, 30% by volume, are pollinated by bees</h1>
+      <p>Most fruits and vegetables are animal pollinated, including chocolate, coffee, tea, and avocados</p>
+      <a href="https://en.wikipedia.org/wiki/List_of_crop_plants_pollinated_by_bees" target="_blank">Click to see all the other crops pollinated by food</a>
+    </article>
+  )
+}
+
+const economicQuestion = () => {
+  <form>
+    <label htmlFor="diet">How much do bees contribute to the economy, anually?</label>
+    <input type="text" id="died" placeholder="Not sure on best avenue to provide answers to these questions"/>
+    <button type="submit">Guess</button>
+  </form>
+}
+
+const economicArticle = () => {
+  return (
+    <article>
+      <h1>The honeybee services are valued to be just above $20 billion in the United States. World wide, that number rises to $217 billion. </h1>
+
+    </article>
+  )
+}
+
+export const Impact = () => {
+  return (
+    <div className="Impact">
+      { dietQuestion() }
+      { dietArticle() }
+      { economicQuestion() }
+      { economicArticle() }
+    </div>
+  )
+};
