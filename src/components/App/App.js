21d772d235c8b9007199fd3ce10ab40063917e5e
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-05 20:04:11 -0600

    Turn switch button into a component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,26 +4,14 @@ import { Impact } from '../Impact/Impact';
 import { Help } from '../Help/Help';
 import { BeeFacts } from '../BeeFacts/BeeFacts';
 import { Problems } from '../Problems/Problems';
+import SwitchButton from '../SwitchButton/SwitchButton';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
-        <div class="remember-me">
-        <input type="checkbox" 
-          name="remember_me" 
-          id="remember_me"
-          checked/>
-        <label for="remember_me">
-          <div class="switch">
-            <span class="dot"></span>
-          </div>
-          Switch&nbsp;&nbsp;&nbsp;&nbsp;
-        </label>
-      </div>
-        
+        <SwitchButton />
         <Header />
         <Impact />
         <BeeFacts />
