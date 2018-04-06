c63a853f8d07b96b6895c38f6364077ffa46b472
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-05 19:13:44 -0600

    Add somewhat functional switch for turing off questions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+        <div class="remember-me">
+        <input type="checkbox" 
+          name="remember_me" 
+          id="remember_me"
+          checked/>
+        <label for="remember_me">
+          <div class="switch">
+            <span class="dot"></span>
+          </div>
+          Switch&nbsp;&nbsp;&nbsp;&nbsp;
+        </label>
+      </div>
+        
         <Header />
         <Impact />
         <BeeFacts />
