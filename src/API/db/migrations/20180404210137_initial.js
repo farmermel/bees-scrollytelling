0bb0ccabda1c31a64437ff649077c74c57af8811
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-08 14:06:13 -0600

    Fix exports down to drop tables in order so that dependent table is dropped first

--- src/API/db/migrations/20180404210137_initial.js
+++ src/API/db/migrations/20180404210137_initial.js
@@ -22,7 +22,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('users'),
-    knex.schema.dropTable('bees')
+    knex.schema.dropTable('bees'),
+    knex.schema.dropTable('users')
   ]);
 };
