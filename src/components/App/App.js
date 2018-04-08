215e0c0f10f3ec2f3f02b8dd25851482254ea10a
Casey Dallavalle <cbdallavalle@gmail.com>
2018-04-08 11:29:11 -0600

    Add parallax and handle scroll Add parallax to text in the header component. Handle users scroll position so that the user must answer the questions before being able to move forward

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,11 +8,35 @@ import SwitchButton from '../SwitchButton/SwitchButton';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scrollPosition: 750
+    }
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.stopScroll);
+  }
+
+  stopScroll = () => {
+    if(window.scrollY > this.state.scrollPosition) {
+      window.scrollTo(0, this.state.scrollPosition)
+    }
+    console.log(window.scrollY)
+
+  }
+
+  startScroll = (e, scrollPosition) => {
+    e.preventDefault();
+    this.setState({ scrollPosition })
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" onScroll={() => console.log('hi')}>
         <SwitchButton />
-        <Header />
+        <Header startScroll={this.startScroll} />
         <Impact />
         <BeeFacts />
         <Problems />
