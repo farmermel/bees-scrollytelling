34535dc279928a31e098ed4e18d8d7cbc6c073d9
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-09 17:56:11 -0600

    Update seed file to have actual foreign key instead of hard coding it

--- src/API/db/seeds/test/initial.js
+++ src/API/db/seeds/test/initial.js
@@ -1,18 +1,18 @@
 exports.seed = function(knex, Promise) {
-  return knex('users')
+  return knex('bees')
     .del()
-    .then(() => knex('bees').del())
+    .then(() => knex('users').del())
     .then(() => {
       return knex('users')
         .insert([
           { location: 'Maryland', age: 10, concern: 'Not very concerned' },
           { location: 'Colorado', age: 10, concern: 'Not very concerned' },
           { location: 'Jersey', age: 10, concern: 'Not very concerned' }
-        ])
-        .then(() => {
+        ], 'id')
+        .then(userId => {
           return knex('bees').insert([
-            { question: 'Do you LOVE bees', user_answer: 'YES', users_id: '1' },
-            { question: 'what about BEES do you love most', user_answer: 'Honey', users_id: '1' }
+            { question: 'Do you LOVE bees', user_answer: 'YES', users_id: userId[0] },
+            { question: 'what about BEES do you love most', user_answer: 'Honey', users_id: userId[0] }
           ]);
         })
         .then(() => console.log('Seeding complete!'))
