9bc6b8bb48697f78bba438b6e1550bfe968939d0
Casey Dallavalle <cbdallavalle@gmail.com>
2018-04-09 21:12:14 -0600

    Add base styling for Help component

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -3,11 +3,13 @@ import './Help.css';
 
 export const Help = () => {
   return (
-    <div>
+    <div className="Help">
       <h1>Buy organic and local</h1>
       <h1>Grow pollinator gardens</h1>
       <p>Be sure to ask if the plants you buy were grown with neonicitinoides</p>
       <h1>Make bee homes</h1>
+      <div className="bee-images bee-home">
+      </div>
       <h1>Support bee friendly organizations and legislation</h1>
     </div>
   );
