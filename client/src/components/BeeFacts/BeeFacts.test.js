f7e715758ec301c8a32ab8636b8f267c39445e51
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-11 14:27:05 -0600

    Write beefacts tests

--- client/src/components/BeeFacts/BeeFacts.test.js
+++ client/src/components/BeeFacts/BeeFacts.test.js
@@ -3,13 +3,59 @@ import { shallow } from 'enzyme';
 import BeeFacts from './BeeFacts';
 
 describe('BeeFacts', () => {
-  it.skip('should match the Snapshot', () => {
-    const wrapper = shallow(<BeeFacts />);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<BeeFacts startScroll={jest.fn()}
+                                questionsEnabled={true}
+                                currentUserId={2} />)
+  })
+
+  it.skip('matches the Snapshot', () => {
     expect(wrapper).to.match.snapshot();
   });
 
-  it('should', () => {
-  
+  it('has default state', () => {
+    expect(wrapper.instance().state).toEqual({beeSpecies: ''});
+  });
+
+  describe('handleChange', () => {
+    const event = {
+      target: {
+        name: 'Jeff', value: 'websocket'
+      }
+    }
+
+    it('sets state with name and value of event passed in', () => {
+      wrapper.instance().handleChange(event);
+
+      expect(wrapper.instance().state.Jeff).toEqual('websocket');
+    });
+  });
+
+  describe('handleSubmit', () => {
+    global.window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({})
+      })
+    })
+
+    const postBody = {"body": "{\"users_id\":2,\"user_answer\":\"\",\"question\":\"bee species\"}", "headers": {"Content-type": "application/json"}, "method": "POST"};
+
+    const event = {};
+
+    it('calls startScroll with event passed in and 6000', () => {
+      wrapper.instance().handleSubmit(event);
+
+      expect(wrapper.instance().props.startScroll).toHaveBeenCalledWith(event, 6000);
+    });
+
+    it('calls fetch with /api/v1/answers and the post body', () => {
+      wrapper.instance().setState({beeSpecies: 'rusty'})
+      wrapper.instance().handleSubmit(event);
+
+      expect(window.fetch).toHaveBeenCalledWith('/api/v1/answers', postBody);
+    });
   });
 });
-
