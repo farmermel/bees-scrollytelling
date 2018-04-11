6ca951a30abd31c51503cc068d1b22dd804046c8
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-11 14:27:28 -0600

    Write header tests

--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -7,64 +7,136 @@ describe('Header', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Header />, { disableLifeCycleMethods: true });
+    wrapper = shallow(<Header startScroll={jest.fn()}
+                              questionsEnabled={true}
+                              saveUser={jest.fn()} />, { disableLifeCycleMethods: true });
+    global.window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({})
+      })
+    })
   })
 
   it.skip('matches the Snapshot', () => {
     expect(wrapper).to.match.snapshot();
   });
 
-  it.skip('has default state', () => {
-    
+  it('has default state', () => {
+    const expected = {
+      concern: '',
+      age: '',
+      location: {}
+    };
+
+    expect(wrapper.instance().state).toEqual(expected);
   });
 
   describe('componentDidMount', () => {
-    it.skip('calls handleCurrentLocation', () => {
+    it('calls handleCurrentLocation', () => {
+      wrapper.instance().handleCurrentLocation = jest.fn();
 
+      expect(wrapper.instance().handleCurrentLocation).not.toHaveBeenCalled();
+
+      wrapper.instance().componentDidMount();
+
+      expect(wrapper.instance().handleCurrentLocation).toHaveBeenCalled();
     });
   });
 
   describe('shouldComponentUpdate', () => {
-    it.skip('returns false if the location of the nextState passed in is different from current location in state', () => {
-
+    it('returns false if the location of the nextState passed in is different from current location in state', () => {
+      expect(wrapper.instance().shouldComponentUpdate({}, {blah: 'blah'})).toEqual(false);
     });
 
-    it.skip('returns true if the location of the nextState passed in matches current locaiton in state', () => {
+    it('returns true if the location of the nextState passed in matches current location in state', () => {
+      const currentState = wrapper.instance().state;
 
+      expect(wrapper.instance().shouldComponentUpdate({}, currentState)).toEqual(true);
     });
   });
 
   describe('handleCurrentLocation', () => {
-    it.skip('calls getCurrentPosition on the geolocation object on the navigator object', () => {
+    beforeEach(() => {
+      const mockGeo = {
+        getCurrentPosition: jest.fn().mockImplementation(() => {
+          return wrapper.instance().setState({ location: {latitude: 4, longitude: 5}})
+        }) 
+      }
 
-    })
+      global.navigator.geolocation = mockGeo;
+    });
 
-    //don't super know how to test this
+    it('calls getCurrentPosition on the geolocation object on the navigator object', async () => {
+      await wrapper.instance().handleCurrentLocation();
+
+      expect(navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
+    });
+
+    it('sets State with current location', async () => {
+      await wrapper.instance().handleCurrentLocation();
+
+      expect(wrapper.instance().state.location).toEqual({"latitude": 4, "longitude": 5});
+    });
   });
 
   describe('handleSubmit', () => {
-    it.skip('calls preventDefault on event passed in', () => {
+    let event;
+    beforeEach(() => {
+      event = {
+          preventDefault: jest.fn()
+      };
+      wrapper.instance().setState({concern: 'yes', age: '12'});  
+    })
 
+    it('calls preventDefault on event passed in', () => {
+      wrapper.instance().handleSubmit(event);
+
+      expect(event.preventDefault).toHaveBeenCalled();
     });
 
-    it.skip('calls startScroll with event and 1396', () => {
+    it('calls startScroll with event and 1396 if concern and age have values in state', () => {
+      wrapper.instance().handleSubmit(event);
 
+      expect(wrapper.instance().props.startScroll).toHaveBeenCalledWith(event, 1396);
     });
 
-    it.skip('calls postToDB', () => {
+    it('calls postToDB', async () => {
+      wrapper.instance().postToDB = jest.fn();
 
+      await wrapper.instance().handleSubmit(event);
+
+      expect(wrapper.instance().postToDB).toHaveBeenCalled();
     });
   });
 
   describe('postToDB', () => {
-    it.skip('calls saveUser with userId', () => {
+    it('calls saveUser with userId', async () => {
+      global.window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          status: 200,
+          json: () => Promise.resolve({ id: 5})
+        })
+      })
+      wrapper.instance().setState({concern: 'yes', age: '12', location: 'here'});
 
+      await wrapper.instance().postToDB();
+
+      expect(wrapper.instance().props.saveUser).toHaveBeenCalledWith(5);
     });
   });
 
   describe('handleChange', () => {
-    it.skip('sets state with name and value of target of event passed in', () => {
+    const event = {
+      target: {
+        name: 'Casey', value: 'good'
+      }
+    }
 
+    it('sets state with name and value of target of event passed in', () => {
+      wrapper.instance().handleChange(event);
+      
+      expect(wrapper.instance().state.Casey).toEqual('good');
     });
   });
 });
