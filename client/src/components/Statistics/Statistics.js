d035eed12c5f45ffe04188c182ef4ef120194dcd
Casey Dallavalle <cbdallavalle@gmail.com>
2018-04-10 20:09:38 -0600

    Upon load statistics for all questions except economic are loaded

--- client/src/components/Statistics/Statistics.js
+++ client/src/components/Statistics/Statistics.js
@@ -7,43 +7,74 @@ class Statistics extends Component {
     this.state = {};
   }
 
-  componentDidMount = () => {
-    this.getStatistics();
+  componentDidMount = async () => {
+    await this.getStatistics();
+    // await this.getAverages();
   }
 
   getStatistics = async () => {
     const output = await fetch('/api/v1/answers');
     const rawResponse = await output.json();
-    const response = this.cleanStatistics(rawResponse)
+    const statsWithoutAverage = this.cleanStatistics(rawResponse);
+    const response = this.getAverages(statsWithoutAverage);
     await this.setState({ response })
   }
 
   cleanStatistics = (response) => {
     return response.reduce( (answers, questionObj, index) => {
       if (!answers[questionObj.question]) {
-        answers[questionObj.question] = {};
+        answers[questionObj.question] = { count: {}, sum: 0 };
       }
 
-      if (!answers[questionObj.question][questionObj.user_answer]) {
-        answers[questionObj.question][questionObj.user_answer] = 0;
+      //handle count
+      if (!answers[questionObj.question].count[questionObj.user_answer]) {
+        answers[questionObj.question].count[questionObj.user_answer] = 0;
       }
 
-      answers[questionObj.question][questionObj.user_answer]++;
+      answers[questionObj.question].count[questionObj.user_answer]++;
+
+      //handle sum
+      if (questionObj.user_answer.split('_').length === 1) {
+        answers[questionObj.question].sum += parseInt(questionObj.user_answer);
+      }
 
       return answers
     }, {})
   }
 
+  getAverages = (response) => {
+    Object.keys(response).map(key => {
+      const counts = Object.values(response[key].count);
+      const totalCount = counts.reduce((totalCount, number) => {return totalCount += number}, 0);
+      response[key].average = response[key].sum / totalCount
+    });
+    return response
+  }
+
+  displayStatistics = () => {
+    const response = this.state.response;
+    console.log(response)
+    if (response) {
+      return Object.keys(response).map( (key, index) => {
+      return (
+        <div>
+          <h1>{key}</h1>
+          <p>{response[key].average}</p>
+        </div>
+      )
+      })
+    }
+  }
+
   render = () => {
+    console.log(this.state)
     return (
       <div className='Statistics'>
         <p>Statistics</p>
+        { this.displayStatistics() }
       </div>
     )
   }
 }
 
 export default Statistics;
-
-
-// {id: 1, question: "Do you LOVE bees", user_answer: "YES", users_id: 1, created_at: "2018-04-08T21:05:26.066Z", …}
