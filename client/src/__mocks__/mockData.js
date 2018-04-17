6c75a9d328ef854f54181dcb8e263c1364090116
Jeff Goodall <goodallhj@gmail.com>
2018-04-17 09:27:37 -0600

    linting with 0 errors

--- client/src/__mocks__/mockData.js
+++ client/src/__mocks__/mockData.js
@@ -7,14 +7,14 @@ module.exports = {
   },
   event: {
     target: {
-      name: 'Jeff', 
+      name: 'Jeff',
       value: 'websocket'
     }
   },
   beeFactsPost: {
-    "body": "{\"users_id\":2,\"user_answer\":\"\",\"question\":\"bee species\"}", 
-    "headers": {"Content-type": "application/json"}, 
-    "method": "POST"
+    body: '{"users_id":2,"user_answer":"","question":"bee species"}',
+    headers: { 'Content-type': 'application/json' },
+    method: 'POST'
   },
   headerDefaultState: {
     concern: '',
@@ -27,10 +27,10 @@ module.exports = {
     monetary: '',
     economy: ''
   },
-  impactFactsPost:  {
-    "body": "{\"users_id\":2,\"question\":\"are plants great\"}", 
-    "headers": {"Content-type": "application/json"}, 
-    "method": "POST"
+  impactFactsPost: {
+    body: '{"users_id":2,"question":"are plants great"}',
+    headers: { 'Content-type': 'application/json' },
+    method: 'POST'
   },
   problemsPostBody: {
     method: 'POST',
@@ -44,26 +44,79 @@ module.exports = {
     })
   },
   rawStatistics: [
-    {id: 48, question: "What percentage of your diet do you think is provided by bees?", user_answer: "35", users_id: 50 },
-    {id: 49, question: "How much do bees contribute to the economy, anually?", user_answer: "400_million", users_id: 50 },
-    {id: 50, question: "bee species", user_answer: "200", users_id: 50 },
-    {id: 51, question: "What percent of bee colonies do beekeepers lose every year?", user_answer: "40", users_id: 50 },
-    {id: 52, question: "What percentage of your diet do you think is provided by bees?", user_answer: "80", users_id: 51 },
-    {id: 53, question: "How much do bees contribute to the economy, anually?", user_answer: "400_billion", users_id: 51 },
-    {id: 54, question: "bee species", user_answer: "100", users_id: 51 },
-    {id: 55, question: "What percent of bee colonies do beekeepers lose every year?", user_answer: "25", users_id: 51 }
+    {
+      id: 48,
+      question:
+        'What percentage of your diet do you think is provided by bees?',
+      user_answer: '35',
+      users_id: 50
+    },
+    {
+      id: 49,
+      question: 'How much do bees contribute to the economy, anually?',
+      user_answer: '400_million',
+      users_id: 50
+    },
+    { id: 50, question: 'bee species', user_answer: '200', users_id: 50 },
+    {
+      id: 51,
+      question: 'What percent of bee colonies do beekeepers lose every year?',
+      user_answer: '40',
+      users_id: 50
+    },
+    {
+      id: 52,
+      question:
+        'What percentage of your diet do you think is provided by bees?',
+      user_answer: '80',
+      users_id: 51
+    },
+    {
+      id: 53,
+      question: 'How much do bees contribute to the economy, anually?',
+      user_answer: '400_billion',
+      users_id: 51
+    },
+    { id: 54, question: 'bee species', user_answer: '100', users_id: 51 },
+    {
+      id: 55,
+      question: 'What percent of bee colonies do beekeepers lose every year?',
+      user_answer: '25',
+      users_id: 51
+    }
   ],
   cleanedStatistics: {
-    'How much do bees contribute to the economy, anually?': {count: {'400_million': 1, '400_billion': 1}, sum: 400400000000},
-    'What percent of bee colonies do beekeepers lose every year?': {count: {25: 1, 40: 1}, sum: 65},
-    'What percentage of your diet do you think is provided by bees?': {count: {35: 1, 80: 1}, sum: 115},
-    'bee species': {count: {100: 1, 200: 1}, sum: 300}
+    'How much do bees contribute to the economy, anually?': {
+      count: { '400_million': 1, '400_billion': 1 },
+      sum: 400400000000
+    },
+    'What percent of bee colonies do beekeepers lose every year?': {
+      count: { 25: 1, 40: 1 },
+      sum: 65
+    },
+    'What percentage of your diet do you think is provided by bees?': {
+      count: { 35: 1, 80: 1 },
+      sum: 115
+    },
+    'bee species': { count: { 100: 1, 200: 1 }, sum: 300 }
   },
   cleanedAverageData: {
-    'How much do bees contribute to the economy, anually?': {count: {'400_million': 1, '400_billion': 1}, sum: 400400000000, average: 200200000000},
-    'What percent of bee colonies do beekeepers lose every year?': {count: {25: 1, 40: 1}, sum: 65, average: 32.5},
-    'What percentage of your diet do you think is provided by bees?': {count: {35: 1, 80: 1}, sum: 115, average: 57.5},
-    'bee species': {count: {100: 1, 200: 1}, sum: 300, average: 150}
+    'How much do bees contribute to the economy, anually?': {
+      count: { '400_million': 1, '400_billion': 1 },
+      sum: 400400000000,
+      average: 200200000000
+    },
+    'What percent of bee colonies do beekeepers lose every year?': {
+      count: { 25: 1, 40: 1 },
+      sum: 65,
+      average: 32.5
+    },
+    'What percentage of your diet do you think is provided by bees?': {
+      count: { 35: 1, 80: 1 },
+      sum: 115,
+      average: 57.5
+    },
+    'bee species': { count: { 100: 1, 200: 1 }, sum: 300, average: 150 }
   },
   pieChartDefaultState: {
     class: '',
@@ -72,8 +125,8 @@ module.exports = {
   },
   percentEvent: {
     target: {
-      name: 'percent', 
+      name: 'percent',
       value: 55
     }
-  },
-}
+  }
+};
